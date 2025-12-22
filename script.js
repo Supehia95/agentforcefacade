822be6bf529dac3da29462ab3d05d7790c35d513
Supehia95 <sandeep.supehia@trufflecorp.com>
2025-12-23 04:13:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,27 +29,31 @@ function initEmbeddedMessaging() {
             messageSentCount++;
             if(messageSentCount>=2){
                botreplied = true;
+           
             }
         });
           window.addEventListener('onEmbeddedMessagingConversationParticipantChanged', function (event) {
             console.log("✅ onEmbeddedMessagingConversationParticipantChanged");
               botloaded =true;
+               const chatWindow = document.querySelector('[data-id="convivaAIChatWindow"]');
+            chatWindow.classList.add("hideContainer");
+            chatWindow.classList.remove("unhideContainer");
         });
         window.addEventListener("onEmbeddedMessagingReady", () => {
             console.log("✅ onEmbeddedMessagingReady");
             setTimeout(() => {
                 if(!isChatOpened){
-                    const chatBubble = document.querySelector('[data-id="convivaAIChatBubble"]');
-                    chatBubble.classList.add("unhideContainer");
-                    chatBubble.classList.remove("hideContainer");
+                    //const chatBubble = document.querySelector('[data-id="convivaAIChatBubble"]');
+                    //chatBubble.classList.add("unhideContainer");
+                    //chatBubble.classList.remove("hideContainer");
                 }
             }, 1500);
         });
         window.addEventListener('onEmbeddedMessagingConversationStarted', function (event) {
             console.log("✅ onEmbeddedMessagingConversationStarted");
-            const chatWindow = document.querySelector('[data-id="convivaAIChatWindow"]');
-            chatWindow.classList.add("hideContainer");
-            chatWindow.classList.remove("unhideContainer");
+           // const chatWindow = document.querySelector('[data-id="convivaAIChatWindow"]');
+           // chatWindow.classList.add("hideContainer");
+            //chatWindow.classList.remove("unhideContainer");
         });
         window.addEventListener('onEmbeddedMessagingFirstBotMessageSent', function (event) {
             console.log("✅ onEmbeddedMessagingFirstBotMessageSent");
@@ -59,15 +63,19 @@ function initEmbeddedMessaging() {
         window.addEventListener('onEmbeddedMessagingConversationOpened', function (event) {
             console.log("✅ onEmbeddedMessagingConversationOpened");
             isChatOpened = true;
-            const chatBubble = document.querySelector('[data-id="convivaAIChatBubble"]');
-            chatBubble.classList.add("hideContainer");
-            chatBubble.classList.remove("unhideContainer");
+            //const chatBubble = document.querySelector('[data-id="convivaAIChatBubble"]');
+            //chatBubble.classList.add("hideContainer");
+            //chatBubble.classList.remove("unhideContainer");
         });
         window.addEventListener('onEmbeddedMessagingWindowClosed', function (event) {
             isChatOpened = false;
             botloaded = false;
             botreplied = false;
             messageSentCount = 0;
+             const quickChatContainer = document.querySelector('[data-id="quickChatContainer"]');
+            quickChatContainer.classList.remove("hideQuickContainer");
+            const quickChatLoadContainer = document.querySelector('[data-id="quickChatLoadContainer"]');
+            quickChatLoadContainer.classList.add("hideQuickContainer");
             
         });
     } catch (err) {
@@ -107,5 +115,11 @@ function onINeedTechnicalSupport(){
     doCallAgentforce();
 }
 function doCallAgentforce() {
+            const quickChatContainer = document.querySelector('[data-id="quickChatContainer"]');
+            quickChatContainer.classList.add("hideQuickContainer");
+            const quickChatLoadContainer = document.querySelector('[data-id="quickChatLoadContainer"]');
+            quickChatLoadContainer.classList.remove("hideQuickContainer");
+
+    
     embeddedservice_bootstrap.utilAPI.launchChat();
 }
